ae625b2e30df338c9133ab432bc6edae810d09d4
Cyrill Burren <cyrill.burren@students.fhnw.ch>
2017-04-17 22:52:30 +0200

    Lotto wurde hinzugefügt

---
+++ Sandbox/src/lotto/LottoController.java
@@ -0,0 +1,95 @@
+package lotto;
+
+import java.net.URL;
+import java.security.cert.CollectionCertStoreParameters;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.TilePane;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
+
+public class LottoController implements Initializable   {
+	
+	@FXML TilePane zahlenPane;
+	@FXML Label choiceAnz;
+	@FXML ProgressBar choiceProgress;
+	
+	
+	//Definition der Spielgrösse
+	private final int MAXNR = 42;
+	private final int MAXCHOICE = 6;
+	
+	int anzGew = 0;
+	int i;
+	
+	private ToggleButton[] toggleArray = new ToggleButton[MAXNR]; 		// Alle Toggels
+	private ArrayList<Integer> userChoice = new ArrayList<Integer>();	// Auswahl des Spielers
+
+	
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		
+		updateChoiceProgress();
+	
+		for ( i = 0; i < MAXNR; i++ ){
+			
+			//Feld zum Auswählen der Zahlen instanzieren
+			toggleArray[i] = new ToggleButton(""+(i+1));
+			toggleArray[i].setId("no"+(i+1));
+			toggleArray[i].getStyleClass().add("zahlenToggles");
+			zahlenPane.getChildren().add(toggleArray[i]);
+			
+			//Definieren des ActionEvents falls die Zahl ausgewählt bzw. abgewählt wird
+			toggleArray[i].setOnAction((event) -> {
+				 ToggleButton sourceTB = (ToggleButton) event.getSource();
+				 Boolean sourceSelected = sourceTB.isSelected();
+				 
+				 if(sourceSelected){ 
+					 if(anzGew < MAXCHOICE){
+						 anzGew++;
+						 //System.out.println(sourceTB.getId()+" selected");
+						 //System.out.println("Gewählt: "+anzGew);
+						 
+						 userChoice.add(Integer.parseInt(sourceTB.getText()));
+						 updateChoiceProgress();
+					 }
+					 else{
+						 System.out.println("Schon alle gewählt!");
+						 sourceTB.setSelected(false);
+					 }
+				 }else{
+					 anzGew--;
+					 userChoice.remove(userChoice.indexOf(Integer.parseInt(sourceTB.getText())));
+					 updateChoiceProgress();
+					 //System.out.println(sourceTB.getId()+" deselected");
+				 } 
+		});
+		}
+
+	
+	}
+	
+	public void updateChoiceProgress(){
+		//Fortschirttsbalken und Anzeige aktualisieren 
+		choiceAnz.setText(anzGew+" / "+MAXCHOICE);
+		 choiceProgress.setProgress((double)anzGew/MAXCHOICE);
+		 
+		 Collections.sort(userChoice);
+		 for(int i : userChoice){
+			 System.out.print(i+"/");
+			 
+		 }
+		 System.out.println();
+		 
+		 
+	};
+	
+
+}
---
+++ Sandbox/src/lotto/Main.java
@@ -0,0 +1,28 @@
+package lotto;
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Lotto.fxml"));
+			Scene scene = new Scene(root);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
