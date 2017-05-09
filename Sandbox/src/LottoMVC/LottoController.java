b8f583bfc73f742ae418920b370d81e35b89258b
Cyrill Burren <cyrill.burren@students.fhnw.ch>
2017-05-09 22:35:17 +0200

    Update TipCollection

--- Sandbox/src/LottoMVC/LottoController.java
+++ Sandbox/src/LottoMVC/LottoController.java
@@ -4,118 +4,193 @@ package LottoMVC;
 import java.util.ArrayList;
 import java.util.Collections;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListCell;
 import javafx.scene.control.ToggleButton;
 
 public class LottoController {
 	final private LottoModel model;
 	final private LottoView view;
 	
+	
 	int anzGew = 0;
+	int anzTips = 0;
+	boolean zusatzChoice = false;
 	private ArrayList<Integer> userChoice = new ArrayList<Integer>();
+	private ObservableList<ArrayList<Integer>> allTips = FXCollections.observableArrayList();
 	
 	public LottoController(LottoModel model, LottoView view) {
 		this.model = model;
 		this.view = view;
 		
-		for(int i = 0; i<view.toggleArray.length;i++){
-			view.toggleArray[i].setOnAction(new clicker());
+		for(int i = 0; i<view.hauptZahlen.length;i++){
+			view.hauptZahlen[i].setOnAction(new clicker());
+		}
+		for(int i = 0; i<view.zusatzZahlen.length; i++){
+			view.zusatzZahlen[i].setOnAction(new clickerZusatz());
 		}
 		
 		view.btnRandom.setOnAction(new randomTip());
-	
+		view.btnTipp.setOnAction(new addTip());
+		view.btnZiehung.setOnAction(new startZiehung());
+		view.btnRemoveTip.setOnAction(new removeTip());
 	}
+	
+	
+	// ************************** EVENT HANDLERS ***************************************
 	
 	
 	public class clicker implements EventHandler<ActionEvent>{
 
 		@Override
 		public void handle(ActionEvent event) {
-			 ToggleButton sourceTB = (ToggleButton) event.getSource();
-			 Boolean sourceSelected = sourceTB.isSelected();
+			 ToggleButton sourceButton = (ToggleButton) event.getSource();
+			 Boolean sourceSelected = sourceButton.isSelected();
 			 
 			 if(sourceSelected){ 
 				 if(anzGew < view.MAXCHOICE){
 					 anzGew++;
-					 userChoice.add(Integer.parseInt(sourceTB.getText()));
+					 userChoice.add(Integer.parseInt(sourceButton.getText()));
 					 updateChoiceProgress();
 				 }
 				 else{
 					 view.lblStatus.setText("Schon alle Gewählt!");
-					 sourceTB.setSelected(false);
+					 sourceButton.setSelected(false);
 				 }
 			 }else{
 				 view.lblStatus.setText("");
 				 anzGew--;
-				 userChoice.remove(userChoice.indexOf(Integer.parseInt(sourceTB.getText())));
+				 userChoice.remove(userChoice.indexOf(Integer.parseInt(sourceButton.getText())));
 				 updateChoiceProgress();
 			 }
 		}
 		
 	}
 	
+	
+	public class clickerZusatz implements EventHandler<ActionEvent>{
+		@Override
+		public void handle(ActionEvent event) {
+			if(view.tGroup.getSelectedToggle() == null)
+				zusatzChoice = false;
+			else
+				zusatzChoice = true;
+			updateChoiceProgress();
+		}
+	}
+	
+	public class addTip implements EventHandler<ActionEvent>{
+		@Override
+		public void handle(ActionEvent event) {
+			if(anzGew >= view.MAXCHOICE){
+				ToggleButton selectedZusatz = (ToggleButton) view.tGroup.getSelectedToggle();
+				userChoice.add(Integer.parseInt(selectedZusatz.getText())*-1);
+				Collections.sort(userChoice);
+				allTips.add((ArrayList<Integer>) userChoice.clone());
+				view.tipList.setItems(allTips);
+				anzTips++;
+				System.out.println(view.tipList.getItems());
+				resetTipField();
+			}else{
+				view.lblStatus.setText("Noch nicht alle zahlen gewählt");
+			}
+		    
+		}
+	}
+	
+	public class startZiehung implements EventHandler<ActionEvent>{
+		@Override
+		public void handle(ActionEvent event) {
+			
+		}
+	}
+	
+	public class removeTip implements EventHandler<ActionEvent>{
+		@Override
+		public void handle(ActionEvent event) {
+			allTips.remove(view.tipList.getSelectionModel().getSelectedItem());
+			view.tipList.setItems(allTips);
+			
+			anzTips--;
+			updateChoiceProgress();
+			
+			for(ArrayList<Integer> i : allTips){
+				for(int y : i){
+					System.out.print(y+"/");
+				}
+				System.out.println();
+				 
+			 }
+			System.out.println("--------------");
+		}
+	}	
+	
 	public class randomTip implements EventHandler<ActionEvent>{
 
 		@Override
 		public void handle(ActionEvent event) {
-			generateRandomTip();
+			//Zufällige Hauptzahlen generieren
+			int random;
+			boolean added;
+			anzGew = 0;
+			userChoice.clear();
+			for(int i = 0; i<view.MAXCHOICE;i++){
+				added = false;
+				while(added==false){
+					random = (int)(Math.random() * view.MAXNR + 1);
+					if(userChoice.contains(random)==false){
+						userChoice.add(random);
+						added = true;
+					}
+				}
+			}
+			
+			//Zufällige Zusatzzahl generieren
+			random = (int)(Math.random() * view.MAXZUSATZ);
+			view.zusatzZahlen[random].setSelected(true);
+	        anzGew = view.MAXCHOICE;
+			zusatzChoice = true;
+			updateChoiceProgress();
+			updateButtonState();
 			
 		}
 		
 	}
 	
-	
-	public void generateRandomTip(){
-		int random;
-		boolean added;
-		anzGew = 0;
-		userChoice.clear();
-		for(int i = 0; i<view.MAXCHOICE;i++){
-			added = false;
-			while(added==false){
-				random = (int)(Math.random() * view.MAXNR + 1);
-				if(userChoice.contains(random)==false){
-					userChoice.add(random);
-					added = true;
-				}
-			}
-		}
-		
-		random = (int)(Math.random() * view.MAXZUSATZ);
-		view.zusatzZahlen[random].setSelected(true);
-	
-		
-		anzGew = view.MAXCHOICE;
-		updateChoiceProgress();
-		setButtonState();
-	}
-	
 	public void updateChoiceProgress(){
-		//Fortschirttsbalken und Anzeige aktualisieren 
-		 //choiceAnz.setText(anzGew+" / "+MAXCHOICE);
-		 //choiceProgress.setProgress((double)anzGew/MAXCHOICE);
-		
-		view.lblOutOf.setText(anzGew+" / "+(view.MAXCHOICE+1));
+		//Auswahl sortieren und Anzeige aktualisieren
+		Collections.sort(userChoice); 
+		 if(zusatzChoice)
+			 view.lblOutOf.setText((anzGew+1)+" / "+(view.MAXCHOICE+1));
+		 else
+			 view.lblOutOf.setText((anzGew)+" / "+(view.MAXCHOICE+1));
 		 
-		 Collections.sort(userChoice);
-		 for(int i : userChoice){
-			 System.out.print(i+"/");
-			 
-		 }
-		 System.out.println(); 
+		 view.lblTips.setText(""+anzTips);
 	}
 	
 	
-	public void setButtonState(){
-		for(int i = 0; i<view.toggleArray.length;i++){
-			view.toggleArray[i].setSelected(false);
+	public void updateButtonState(){
+		for(int i = 0; i<view.hauptZahlen.length;i++){
+			view.hauptZahlen[i].setSelected(false);
 		}
 		
 		for(int id:userChoice){
-			view.toggleArray[id-1].setSelected(true);
+			view.hauptZahlen[id-1].setSelected(true);
 		}
+	}
+	
+	public void resetTipField(){
+		userChoice.clear();
+		anzGew = 0;
+		view.tGroup.getSelectedToggle().setSelected(false);
+		zusatzChoice = false;
+		updateChoiceProgress();
+		updateButtonState();
+		
 	}
 	
 
--- Sandbox/src/LottoMVC/LottoView.java
+++ Sandbox/src/LottoMVC/LottoView.java
@@ -5,6 +5,7 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import javafx.scene.control.ToggleButton;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.BorderPane;
@@ -22,9 +23,8 @@ public class LottoView {
 	final int MAXNR = 42;
 	final int MAXZUSATZ = 6;
 	final int MAXCHOICE = 6;
-	final int MAXCHOICEZUSATZ = 1;
 	
-	ToggleButton[] toggleArray = new ToggleButton[MAXNR];
+	ToggleButton[] hauptZahlen = new ToggleButton[MAXNR];
 	ToggleButton[] zusatzZahlen = new ToggleButton[MAXZUSATZ];
 	
 	ToggleGroup tGroup;
@@ -40,28 +40,45 @@ public class LottoView {
 	Button btnRandom;
 	Button btnTipp;
 	Button btnZiehung;
+	Button btnRemoveTip;
 	
+	BorderPane bPane;
+	GridPane gpCenter;
+	
+	TilePane tPane;
+	TilePane tPaneZusatz;
+	
+	VBox vbRight;
+	VBox vbCenter;
+	VBox vbLeft;
+	
+	HBox buttonBox;
+	
+	
+	ListView tipList;
 	
 	public LottoView(Stage stage, LottoModel model){
 		this.model = model;
 		this.stage = stage;
 		
-		BorderPane bPane = new BorderPane();
-		VBox vbCenter = new VBox();
+		bPane = new BorderPane();
+		
+		// ************************** MITTE  ******************************
+		
+		vbCenter = new VBox();
 		vbCenter.setAlignment(Pos.CENTER);
 		vbCenter.setSpacing(30);
-		vbCenter.setMaxWidth(800);
 		
-		TilePane tPane = new TilePane();
+		tPane = new TilePane();
 		tPane.setAlignment(Pos.CENTER);
 		tPane.setMaxSize(280, 700);
 		tPane.setMinSize(260,150);
 		
-		TilePane tPaneZusatz = new TilePane();
+		tPaneZusatz = new TilePane();
 		tPaneZusatz.setAlignment(Pos.CENTER);
 		tPaneZusatz.setMaxSize(280, 700);
 		
-		HBox buttonBox = new HBox();
+		buttonBox = new HBox();
 		buttonBox.setAlignment(Pos.CENTER);
 		buttonBox.setSpacing(15);
 		
@@ -78,11 +95,11 @@ public class LottoView {
 		
 		//********************** LINKS ************************
 		
-		VBox vbLeft = new VBox();
+		vbLeft = new VBox();
 		vbLeft.setAlignment(Pos.CENTER);
-		lblOutOf = new Label ("" + MAXCHOICE);
+		lblOutOf = new Label ("0 / " + MAXCHOICE);
 		lblOutOfText = new Label("Zahlen gewählt:");
-		lblTips = new Label ("2");
+		lblTips = new Label ("0");
 		lblTipsText = new Label ("Gesetzte Tipps:");
 		lblMoney = new Label ("2500.- CHF");
 		lblMoneyText = new Label ("Kontostand:");
@@ -93,22 +110,32 @@ public class LottoView {
 		lblMoney.setStyle("-fx-font-size: 22px;");
 		vbLeft.getChildren().addAll(lblOutOfText, lblOutOf, lblTipsText, lblTips, lblMoneyText, lblMoney);
 		
-		GridPane gpCenter = new GridPane();
+		
+		
+		//********************* RECHTS ********************************
+		vbRight = new VBox();
+		vbRight.setAlignment(Pos.CENTER);
+		tipList = new ListView();
+		btnRemoveTip = new Button("Tipp entfernen");
+		vbRight.getChildren().addAll(tipList, btnRemoveTip);
+		
+		
+		gpCenter = new GridPane();
 		gpCenter.setAlignment(Pos.CENTER);
 		gpCenter.setConstraints(vbLeft, 0, 1);
 		gpCenter.setConstraints(vbCenter, 1, 1);
+		gpCenter.setConstraints(vbRight,2,1);
 		
 		ColumnConstraints col1 = new ColumnConstraints();
-	    col1.setPercentWidth(33);
+	    col1.setPercentWidth(25);
 	    col1.setHalignment(HPos.LEFT);
 	    ColumnConstraints col2 = new ColumnConstraints();
-	    col2.setPercentWidth(33);
+	    col2.setPercentWidth(50);
 	    ColumnConstraints col3 = new ColumnConstraints();
-	    col3.setPercentWidth(33);
+	    col3.setPercentWidth(25);
 	    gpCenter.getColumnConstraints().addAll(col1,col2,col3);
 		
-		gpCenter.getChildren().addAll(vbLeft,vbCenter);
-		
+		gpCenter.getChildren().addAll(vbLeft,vbCenter,vbRight);
 		
 		
 		bPane.setAlignment(gpCenter, Pos.CENTER);
@@ -116,10 +143,10 @@ public class LottoView {
 		
 		for (int i = 0; i < MAXNR; i++){
 			//Feld zum Auswählen der Zahlen instanzieren
-			toggleArray[i] = new ToggleButton(""+(i+1));
-			toggleArray[i].setId("no"+(i+1));
-			toggleArray[i].getStyleClass().add("zahlenToggles");
-			tPane.getChildren().add(toggleArray[i]);
+			hauptZahlen[i] = new ToggleButton(""+(i+1));
+			hauptZahlen[i].setId("no"+(i+1));
+			hauptZahlen[i].getStyleClass().add("zahlenToggles");
+			tPane.getChildren().add(hauptZahlen[i]);
 		}
 		
 		tGroup = new ToggleGroup();
@@ -136,12 +163,6 @@ public class LottoView {
 		
 		stage.setScene(scene);
 		stage.setTitle("Lotto");
-	}
-	
-	public void generateButtons(int anz){
-		for(int i = 0; i<anz; i++){
-			
-		}
 	}
 	
 	public void start() {
